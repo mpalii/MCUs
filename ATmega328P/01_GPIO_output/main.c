79ee3a2fc91288bf3a69eb55786c67f7274cfe8a
Maksym Palii <maksympalii.dev@gmail.com>
2022-02-08 18:39:22 +0200

    328p draft

--- ATmega328P/01_GPIO_output/main.c
+++ ATmega328P/01_GPIO_output/main.c
@@ -2,19 +2,24 @@
  * Created: 18.12.2021 16:04:24
  * Author : Maksym Palii
  */ 
+#define __AVR_ATmega328P__
+#define F_CPU (1000000UL)
 
 #include <avr/io.h>
+#include <util/delay.h>
 
 int main(void)
 {
 	// Configure data direction for pin 0 (port D) as OUTPUT
 	DDRD |= (1 << DDD0);
 	
-	// Set pin 0 (port D) driven high (one)
-	PORTD |= (1 << PORTD0);
-	
 	while (1) 
 	{
-		// No logic
+		for (char i = 0; i < 6; i++)
+		{
+			PORTD ^= (1 << PD0);
+			_delay_ms(125);
+		}
+		_delay_ms(5000);
 	}
 }
